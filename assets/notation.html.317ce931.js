9588ea5b7bd3595fbe28b60280b1e4678f4e112c
Tim <tim.r.burr@gmail.com>
2021-12-13 07:29:02 -0700

    re-org discord guide

---
+++ assets/notation.html.317ce931.js
@@ -0,0 +1,1 @@
+const e={key:"v-cc3e5cfa",path:"/additional-info/notation.html",title:"Understanding notation",lang:"en-US",frontmatter:{},excerpt:"",headers:[{level:2,title:"Classes",slug:"classes",children:[]},{level:2,title:"Types",slug:"types",children:[]}],filePathRelative:"additional-info/notation.md",git:{updatedTime:1627592656e3}};export{e as data};
